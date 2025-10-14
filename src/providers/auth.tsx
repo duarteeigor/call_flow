b66074960407ef5f978671a06c0b2aba5cd61351
duarteeigor <duarteigor562@gmail.com>
2025-10-14 10:20:08 -0300

    feat: creating client component receiving session provider

---
+++ src/providers/auth.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { SessionProvider, type SessionProviderProps } from "next-auth/react"
+
+export const AuthProvider = ({children} : SessionProviderProps)=>{
+    return(
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
