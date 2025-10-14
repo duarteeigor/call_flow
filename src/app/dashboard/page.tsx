201878f8df578b2aea812da29314d3b8ba91ded8
duarteeigor <duarteigor562@gmail.com>
2025-10-14 14:20:50 -0300

    feat: protecting dashboard with new auth from nextauth v5

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
-
+import {auth} from "@/auth"
+import { redirect } from "next/navigation"
 
 export default async function Dashboard(){
-   
+    const session = await auth()
+    if(!session){
+        redirect("/")
+    }
    
     return(
         <div>
