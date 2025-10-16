b19d698bcad9542094ab68180c7abaa187a48c41
duarteeigor <duarteigor562@gmail.com>
2025-10-16 18:03:29 -0300

    feat: creating file types

---
+++ src/utils/types/costumer.ts
@@ -0,0 +1,8 @@
+export interface CostumerProps {
+        id: string;
+        address?: string | null;
+        name: string;
+        phone: string;
+        email: string;
+        user_id: string | null;
+}
