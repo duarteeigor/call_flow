5dd98a2ce1dd83de0352df738844f448a316e9b6
duarteeigor <duarteigor562@gmail.com>
2025-10-16 19:16:52 -0300

    feat: creating type for ticket

---
+++ src/utils/types/ticket.ts
@@ -0,0 +1,13 @@
+import { CostumerProps } from "./costumer";
+
+export interface TicketProps{
+    name: string;
+    id: string;
+    created_at: Date | null;
+    updated_at: Date | null;
+    user_id: string | null;
+    description: string;
+    status: string;
+    costumer_id: string | null;
+    costumer: CostumerProps
+}
