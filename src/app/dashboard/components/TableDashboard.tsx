3de70b7134438e4ebd821d6a1b56d944289532c1
duarteeigor <duarteigor562@gmail.com>
2025-10-17 12:53:14 -0300

    feat: adding feature to change status of the ticket

--- src/app/dashboard/components/TableDashboard.tsx
+++ src/app/dashboard/components/TableDashboard.tsx
@@ -1,9 +1,29 @@
 "use client"
 
+
 import { TicketProps } from "@/utils/types/ticket"
 import { CircleCheckBigIcon, Pencil } from "lucide-react"
+import { useRouter } from "next/navigation"
+
 
 export function TableDashboard({tickets}: {tickets: TicketProps[]}){
+     const router = useRouter()
+     
+
+     async function handleChangeStatus(id: string){
+        
+        await fetch(`/api/ticket`, {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application.json"
+            },
+            body: JSON.stringify({
+                id
+            })
+        })
+
+        router.refresh()
+    }
     return(
         <table className="w-full">
                 <thead className="">
@@ -20,13 +40,13 @@ export function TableDashboard({tickets}: {tickets: TicketProps[]}){
                         <tr key={ticket.id}
                             className="border-b-2 border-b-slate-200 h-16  last:border-b-0 bg-slate-100 
                             hover:bg-slate-200 transition-colors duration-300 cursor-pointer">
-                            <td className="px-4 py-2 text-left">{ticket.name}</td>
+                            <td className="px-4 py-2 text-left">{ticket.costumer?.name}</td>
                             <td className="px-4 py-2 text-left hidden sm:table-cell">{ticket.created_at?.toLocaleDateString("pt-br")}</td>
-                            <td className="px-4 py-2 text-left"><span className="p-2 bg-[#B4E8D7] text-sm font-medium rounded-md">{ticket.status.toUpperCase()}</span></td>
+                            <td className="px-4 py-2 text-left"><span className={`p-2 text-sm font-medium rounded-md ${ticket.status === "aberto" ? "bg-[#B4E8D7]" : "bg-red-500"}`}>{ticket.status.toUpperCase()}</span></td>
                             <td className="px-4 py-2 text-left">
                                 <div className="flex gap-2">
-                                    <CircleCheckBigIcon size={22} color="gray" />
-                                    <Pencil size={22} />
+                                    <CircleCheckBigIcon size={22} color="gray" onClick={()=>handleChangeStatus(ticket.id)} />
+                                    <Pencil  size={22} />
                                 </div>
                             </td>
                         </tr>
