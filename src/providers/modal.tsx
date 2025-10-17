b6243c4a3771076500fe254c5d41455f9fc0f0bc
duarteeigor <duarteigor562@gmail.com>
2025-10-17 12:48:45 -0300

    feat: creating function to get ticket inside the context

--- src/providers/modal.tsx
+++ src/providers/modal.tsx
@@ -1,24 +1,32 @@
 "use client"
 import { Modal } from "@/components/modal/Modal";
+import { TicketProps } from "@/utils/types/ticket";
 import { createContext, useState } from "react";
 
 
 interface ModalContextProps{
     visible: boolean
     changeVisible: ()=> void;
+    getTicket: (data: TicketProps) =>void;
+    ticket: TicketProps | undefined
 }
 
-const ModalContext = createContext({} as ModalContextProps)
+export const ModalContext = createContext({} as ModalContextProps)
 
 export const ModalProvider = ({children}: {children: React.ReactNode})=>{
-    const [visible, setVisible] = useState(true)
+    const [visible, setVisible] = useState(false)
+    const [ticket, setTicket] = useState<TicketProps | undefined>()
 
     function changeVisible(){
         setVisible(!visible)
     }
 
+    function getTicket(data: TicketProps){
+        setTicket(data)
+    }
+
     return(
-        <ModalContext.Provider value={{visible, changeVisible}}>
+        <ModalContext.Provider value={{visible, changeVisible, getTicket, ticket}}>
             {visible && <Modal />}
             {children}
         </ModalContext.Provider>
