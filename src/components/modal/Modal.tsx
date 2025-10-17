c18397baf795f6099b3461176a7627e27f01fba5
duarteeigor <duarteigor562@gmail.com>
2025-10-17 11:07:16 -0300

    feat: creating modal component structure

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+export function Modal(){
+    return(
+        <section className="absolute w-full min-h-screen bg-black/70">
+            <div className="absolute inset-0 flex justify-center items-center">
+
+                <div className="bg-white shadow-lg w-4/5 md:w-1/2 max-w-2xl p-3 rounded">
+                    <p></p>
+                </div>
+            </div>
+
+        </section>
+    )
+}
