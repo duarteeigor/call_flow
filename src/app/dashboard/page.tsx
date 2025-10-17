798d338f0b0079649aa007d687ba7ea3920ad019
duarteeigor <duarteigor562@gmail.com>
2025-10-17 00:46:16 -0300

    refactor: extracting table to a new component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,11 @@
 import { auth } from "@/auth"
 import { redirect } from "next/navigation"
 
-import { Trash, Pencil } from "lucide-react"
+import { CircleCheckBigIcon, Pencil } from "lucide-react"
 import Link from "next/link"
 import { prisma } from "@/lib/prisma"
 import { TicketProps } from "@/utils/types/ticket"
+import { TableDashboard } from "./components/TableDashboard"
 
 export default async function Dashboard() {
     const session = await auth()
@@ -36,34 +37,7 @@ export default async function Dashboard() {
                 </Link>
             </div>
 
-            <table className="w-full">
-                <thead className="">
-                    <tr className="">
-                        <th className="px-4 py-2 text-left font-semibold">CLIENTE</th>
-                        <th className="px-4 py-2 text-left font-semibold hidden sm:table-cell">DATA CADASTRO</th>
-                        <th className="px-4 py-2 text-left font-semibold">STATUS</th>
-                        <th className="px-4 py-2 text-left font-semibold">#</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {tickets && tickets.map((ticket) => (
-                        <tr key={ticket.id}
-                            className="border-b-2 border-b-slate-200 h-16  last:border-b-0 bg-slate-100 
-                            hover:bg-slate-200 transition-colors duration-300 cursor-pointer">
-                            <td className="px-4 py-2 text-left">{ticket.name}</td>
-                            <td className="px-4 py-2 text-left hidden sm:table-cell">{ticket.created_at?.toLocaleDateString("pt-br")}</td>
-                            <td className="px-4 py-2 text-left"><span className="p-2 bg-[#B4E8D7] text-sm font-medium rounded-md">{ticket.status.toUpperCase()}</span></td>
-                            <td className="px-4 py-2 text-left">
-                                <div className="flex gap-2">
-                                    <Trash size={22} color="red" />
-                                    <Pencil size={22} />
-                                </div>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <TableDashboard tickets={tickets} />
         </div>
     )
 }
