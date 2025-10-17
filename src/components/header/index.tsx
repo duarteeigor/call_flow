188aa9e54526314e0b7fcd4ddfdb3787d8460eb2
duarteeigor <duarteigor562@gmail.com>
2025-10-17 20:11:35 -0300

    feat: adding link to home

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -20,44 +20,44 @@ export function Header() {
         <header className={`w-full h-20 flex items-center ${isHome && "bg-transparent fixed"}  `}>
             <div className="max-w-7xl w-full mx-auto flex items-center justify-between px-4">
 
-                {isHome ?(
-                   <>
-                    <Link href={"/"}>
-                        <h1 className={`text-2xl ${isHome ? "text-white" : "text-black"} font-semibold  hover:tracking-widest duration-200 md:text-4xl`}><span className="text-[#1D4ED8]">Call</span>Flow</h1>
-                    </Link>
+                {isHome ? (
+                    <>
+                        <Link href={"/"}>
+                            <h1 className={`text-4xl ${isHome ? "text-white" : "text-black"} font-semibold  hover:tracking-widest duration-200`}><span className="text-[#1A2B42]">Call</span>Flow</h1>
+                        </Link>
 
-                    {status === "loading" && (
-                    <button className="animate-spin">
-                        <LoaderCircle color={isHome ? "#fff" : "#000"} size={24} />
-                    </button>
-                )}
+                        {status === "loading" && (
+                            <button className="animate-spin">
+                                <LoaderCircle color={isHome ? "#fff" : "#000"} size={24} />
+                            </button>
+                        )}
 
-                {status === "unauthenticated" && (
-                    <button className="hover:scale-125 duration-200 cursor-pointer">
-                        <Lock color={isHome ? "#fff" : "#000"} size={24} onClick={() => signIn("google", { redirectTo: "/dashboard" })} />
-                    </button>
-                )}
+                        {status === "unauthenticated" && (
+                            <button className="hover:scale-125 duration-200 cursor-pointer">
+                                <Lock color={isHome ? "#fff" : "#000"} size={24} onClick={() => signIn("google", { redirectTo: "/dashboard" })} />
+                            </button>
+                        )}
 
-                {status === "authenticated" && (
-                    <div className="flex gap-6 items-center">
-                        <button className="hover:scale-125 duration-200 cursor-pointer">
-                            <User color={isHome ? "#fff" : "#000"} size={24} />
-                        </button>
+                        {status === "authenticated" && (
+                            <div className="flex gap-6 items-center">
+                                <button className="hover:scale-125 duration-200 cursor-pointer">
+                                    <Link href={"/dashboard"}><User color={isHome ? "#fff" : "#000"} size={24} /></Link>
+                                </button>
 
-                        <button className="hover:scale-125 duration-200 cursor-pointer">
-                            <LogOut color={isHome ? "#fff" : "#000"} size={24} onClick={() => signOut()} />
-                        </button>
-                    </div>
-                )}
-                   </>
+                                <button className="hover:scale-125 duration-200 cursor-pointer">
+                                    <LogOut color={isHome ? "#fff" : "#000"} size={24} onClick={() => signOut()} />
+                                </button>
+                            </div>
+                        )}
+                    </>
                 ) : (
                     <Sidebar path={path} />
                 )}
-                    
-                    
 
 
-                
+
+
+
 
             </div>
         </header>
