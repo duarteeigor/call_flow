eadf0f9915902131053a3468935ed2468a79ab56
duarteeigor <duarteigor562@gmail.com>
2025-10-21 15:04:50 -0300

    feat: add pop up

--- src/app/dashboard/costumer/components/cardCostumer.tsx
+++ src/app/dashboard/costumer/components/cardCostumer.tsx
@@ -2,41 +2,52 @@
 
 import { CostumerProps } from "@/utils/types/costumer";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 
-export function CardCostumer({ costumer }: {costumer: CostumerProps}) {
+export function CardCostumer({ costumer }: { costumer: CostumerProps }) {
     const router = useRouter()
 
-    async function handleDelete(){
-        await fetch(`/api/costumer?id=${costumer.id}`, {
-            method: "DELETE",
+    async function handleDelete() {
+        try {
+            const response = await fetch(`/api/costumer?id=${costumer.id}`, {
+                method: "DELETE",
+
+            })
+            if(!response.ok){
+                toast.error("Não é possível excluir um cliente com chamados em aberto")
+                return
+            }
             
-        })
-        router.refresh()
+            router.refresh()
+            toast.success("Cliente excluido com sucesso!")
+        } catch (error) {
+            throw new Error("erro")
+        }
     }
 
     return (
         <>
-            
-                <article key={costumer.id} className=" flex flex-col gap-1 bg-slate-100 shadow-md p-3 rounded-md">
-                    <div className="flex gap-2">
-                        <strong>Nome:</strong>
-                        <span>{costumer.name}</span>
-                    </div>
 
-                    <div className="flex gap-2">
-                        <strong>Email:</strong>
-                        <span>{costumer.email}</span>
-                    </div>
+            <article key={costumer.id} className=" flex flex-col gap-1 bg-slate-100 shadow-md p-3 rounded-md">
+                <div className="flex gap-2">
+                    <strong>Nome:</strong>
+                    <span>{costumer.name}</span>
+                </div>
 
-                    <div className="flex gap-2">
-                        <strong>Telefone:</strong>
-                        <span>{costumer.phone}</span>
-                    </div>
+                <div className="flex gap-2">
+                    <strong>Email:</strong>
+                    <span>{costumer.email}</span>
+                </div>
 
-                    <button onClick={handleDelete} className="py-1 px-4 self-start text-sm text-white bg-red-500 rounded-md mt-2 hover:scale-105 duration-200  cursor-pointer">Deletar</button>
-                </article>
-            
+                <div className="flex gap-2">
+                    <strong>Telefone:</strong>
+                    <span>{costumer.phone}</span>
+                </div>
+
+                <button onClick={handleDelete} className="py-1 px-4 self-start text-sm text-white bg-red-500 rounded-md mt-2 hover:scale-105 duration-200  cursor-pointer">Deletar</button>
+            </article>
+
         </>
 
     )
